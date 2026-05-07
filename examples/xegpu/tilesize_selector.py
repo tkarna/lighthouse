b8f58e52ec5ee4710a61705cad012180c35f7204
Tuomas Karna <tuomas.karna@intel.com>
2026-05-07 12:15:27 +0300

    tmp: start adding cost model / tile size selector

---
+++ examples/xegpu/tilesize_selector.py
@@ -0,0 +1,468 @@
+from itertools import product
+import math
+
+from lighthouse.schedule.xegpu.mlp_schedule import (
+    PFETCH_MAX_COLS,
+    PFETCH_MAX_ROWS,
+    PFETCH_MIN_COLS,
+    PFETCH_MIN_ROWS,
+    DPAS,
+)
+from tune_matmul_gridsearch import check_constraints
+
+# GPU specs
+
+# Intel Arc B70
+gpu_specs_B70 = {
+    "name": "Intel Arc B70",
+    "nb_xe_cores": 32,
+    "peak_flops": 155000e9,  # float16
+    "bw_global_mem": 608e9,  # GB/s
+    "bw_local_mem": 3600e9,  # GB/s ???
+    "local_mem_size": 128 * 1024,  # B; both SLM by ecore and max SLM per workgroup
+    "max_nb_threads": 32,  # large register file
+    # "max_nb_threads": 64  # small register file
+    "dpas_exec_size": 16,  # number of parallel dpas ops in sg
+    "nb_registers": 256,  # large register file
+}
+
+# Intel Arc B580
+gpu_specs_B580 = {
+    "name": "Intel Arc B580",
+    "nb_xe_cores": 20,
+    "peak_flops": 100000e9,  # float16
+    "bw_global_mem": 456e9,  # GB/s
+    "bw_local_mem": 3600e9,  # GB/s ???
+    "local_mem_size": 128 * 1024,  # B; both SLM by ecore and max SLM per workgroup
+    "max_nb_threads": 32,  # large register file
+    # "max_nb_threads": 64,  # small register file
+    "dpas_exec_size": 16,  # number of parallel dpas ops in sg
+    "nb_registers": 256,  # large register file
+}
+
+gpu_specs_db = {
+    "B70": gpu_specs_B70,
+    "B580": gpu_specs_B580,
+}
+
+# Tile size selection strategy
+# WG level
+# - Saturate global memory bandwidth.
+# - Fit in SLM.
+# SG level
+# - Saturate local memory bandwidth.
+# - Fit in register file.
+
+
+def get_int_factors(n):
+    "Return a and b such that n = a * b."
+    factors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            a = i
+            b = n // i
+            factors.append((a, b))
+            factors.append((b, a))
+    # sort by how close to square the factors are
+    factors.sort(key=lambda x: abs(x[0] - x[1]))
+    return factors
+
+
+def check_prefetch_tile(tile, data_shape, gpu_specs, name="A"):
+    shape = data_shape
+    if tile[0] < PFETCH_MIN_ROWS:
+        raise ValueError(
+            f"Prefetch tile {name} {tile} has too few rows (min {PFETCH_MIN_ROWS})."
+        )
+    if tile[0] > PFETCH_MAX_ROWS:
+        raise ValueError(
+            f"Prefetch tile {name} {tile} has too many rows (max {PFETCH_MAX_ROWS})."
+        )
+    if tile[1] < PFETCH_MIN_COLS:
+        raise ValueError(
+            f"Prefetch tile {name} {tile} has too few cols (min {PFETCH_MIN_COLS})."
+        )
+    if tile[1] > PFETCH_MAX_COLS:
+        raise ValueError(
+            f"Prefetch tile {name} {tile} has too many cols (max {PFETCH_MAX_COLS})."
+        )
+    if shape[0] % tile[0] != 0 or shape[1] % tile[1] != 0:
+        raise ValueError(
+            f"Prefetch tile {name} {tile} does not divide the parent shape {shape}."
+        )
+    rows = int(shape[0] / tile[0])
+    cols = int(shape[1] / tile[1])
+    nb_threads = int(rows * cols)
+    if nb_threads > gpu_specs["max_nb_threads"]:
+        raise ValueError(
+            f"Number of threads for {name} prefetch ({nb_threads}) exceeds max threads ({gpu_specs['max_nb_threads']})."
+        )
+    return rows, cols
+
+
+def check_prefetch_tile_a(tile, wg_tile, k_tile, gpu_specs):
+    data_shape = (wg_tile[0], k_tile)
+    return check_prefetch_tile(tile, data_shape, gpu_specs, name="A")
+
+
+def check_prefetch_tile_b(tile, wg_tile, k_tile, gpu_specs):
+    data_shape = (k_tile, wg_tile[1])
+    return check_prefetch_tile(tile, data_shape, gpu_specs, name="B")
+
+
+def generate_prefetch_tiles(wg_tile, k_tile, gpu_specs, n=None):
+    """Generates valid prefetch tile sizes for A and B.
+
+    Candidates are sorted by number of threads (descending) and then by how
+    balanced the thread grid is (descending).
+    """
+
+    def gridsearch(check_fn):
+        tiles = []
+        for rows in range(PFETCH_MIN_ROWS, PFETCH_MAX_ROWS + 1):
+            for cols in range(PFETCH_MIN_COLS, PFETCH_MAX_COLS + 1):
+                tile = (rows, cols)
+                try:
+                    grid = check_fn(tile, wg_tile, k_tile, gpu_specs)
+                    nb_threads = int(grid[0] * grid[1])
+                    tiles.append((tile, nb_threads, grid))
+                except ValueError:
+                    pass
+        # sort by number of threads and then by how balanced the thread grid is
+        tiles.sort(key=lambda x: (x[1], -abs(x[2][0] - x[2][1])), reverse=True)
+        tiles = [t[0] for t in tiles]
+        return tiles
+
+    prefetch_tiles_a = gridsearch(check_prefetch_tile_a)
+    prefetch_tiles_b = gridsearch(check_prefetch_tile_b)
+    if n is not None:
+        if n == 1:
+            prefetch_tiles_a = prefetch_tiles_a[0]
+            prefetch_tiles_b = prefetch_tiles_b[0]
+        else:
+            prefetch_tiles_a = prefetch_tiles_a[:n]
+            prefetch_tiles_b = prefetch_tiles_b[:n]
+
+    return prefetch_tiles_a, prefetch_tiles_b
+
+
+def estimate_perf(
+    M,
+    N,
+    K,
+    wg_tile,
+    sg_tile,
+    k_tile,
+    gpu_specs,
+    prefetch_tile_a=None,
+    prefetch_tile_b=None,
+    verbose=True,
+):
+    if verbose:
+        print("=== Global Level ===")
+        print(f"Matrix sizes: M={M}, N={N}, K={K}")
+
+    # TODO generalize
+    ab_dtype_size = 2  # bytes for f16
+    c_dtype_size = 4  # bytes for f32
+
+    # WG
+    if verbose:
+        print("=== Workgroup Level ===")
+    roofline_threshold = (
+        gpu_specs["peak_flops"] / gpu_specs["bw_global_mem"]
+    )  # in FLOPs/Byte
+
+    wg_grid = (M // wg_tile[0], N // wg_tile[1])
+    nb_wgs = wg_grid[0] * wg_grid[1]
+    if verbose:
+        print(f"Workgroup tile size: {wg_tile}, grid size: {wg_grid}, nb WGs: {nb_wgs}")
+        print(f"K tile size: {k_tile}")
+
+    A_wg_shape = (wg_tile[0], k_tile)
+    B_wg_shape = (k_tile, wg_tile[1])
+    C_wg_shape = (wg_tile[0], wg_tile[1])
+
+    A_footprint = A_wg_shape[0] * A_wg_shape[1] * ab_dtype_size
+    B_footprint = B_wg_shape[0] * B_wg_shape[1] * ab_dtype_size
+    C_footprint = C_wg_shape[0] * C_wg_shape[1] * c_dtype_size
+
+    if verbose:
+        print(f"A: shape={A_wg_shape}, footprint={A_footprint / 1024:.2f} KB")
+        print(f"B: shape={B_wg_shape}, footprint={B_footprint / 1024:.2f} KB")
+        print(f"C: shape={C_wg_shape}, footprint={C_footprint / 1024:.2f} KB")
+
+    total_footprint = A_footprint + B_footprint
+    if verbose:
+        print(
+            f"Total SLM footprint: {total_footprint / 1024:.1f} / {gpu_specs['local_mem_size'] / 1024:.1f} KB"
+        )
+
+    if total_footprint > gpu_specs["local_mem_size"]:
+        raise ValueError("SLM footprint exceeds local memory size.")
+
+    # flops = 2 * wg_tile[0] * wg_tile[1] * k_tile
+    # print(f"FLOPs per tile: {flops/1e6:.2f} MFLOPs")
+
+    # arithmetic intensity
+    f = (wg_tile[0] * wg_tile[1]) / (wg_tile[0] + wg_tile[1])
+    ai = f * ab_dtype_size
+    if verbose:
+        print(f"Arithmetic intensity: {ai:.2f} FLOPs/Byte")
+        print(f"Roofline threshold:   {roofline_threshold:.2f} FLOPs/Byte")
+
+    # is this compute or bandwidth bound?
+    if verbose:
+        if ai < roofline_threshold:
+            print(" => Bandwidth-bound regime")
+        else:
+            print(" => Compute-bound regime")
+
+    xe_core_utilization = min(nb_wgs / gpu_specs["nb_xe_cores"], 1.0)
+    if verbose:
+        print(f"XE core utilization: {xe_core_utilization:.2f}")
+
+    # predicted flops
+    peak_flops = (
+        gpu_specs["peak_flops"] * xe_core_utilization
+    )  # possible underutilization
+    predicted_throughput = min(peak_flops, ai * gpu_specs["bw_global_mem"])
+    if verbose:
+        print(f"Predicted throughput: {predicted_throughput / 1e12:.2f} TFLOPS")
+
+    # SG
+    if verbose:
+        print("=== Subgroup Level ===")
+    roofline_threshold = (
+        gpu_specs["peak_flops"] / gpu_specs["bw_local_mem"]
+    )  # in FLOPs/Byte
+
+    sg_grid = (wg_tile[0] // sg_tile[0], wg_tile[1] // sg_tile[1])
+    nb_sgs = sg_grid[0] * sg_grid[1]
+    if verbose:
+        print(
+            f"Subgroup tile size: {sg_tile}, grid size: {sg_grid}, nb SGs per WG: {nb_sgs}"
+        )
+
+    if nb_sgs > gpu_specs["max_nb_threads"]:
+        raise ValueError(
+            f"Number of SGs per WG ({nb_sgs}) exceeds max threads ({gpu_specs['max_nb_threads']})."
+        )
+
+    A_sg_shape = (sg_tile[0], k_tile)
+    B_sg_shape = (k_tile, sg_tile[1])
+    C_sg_shape = (sg_tile[0], sg_tile[1])
+
+    A_footprint = A_sg_shape[0] * A_sg_shape[1] * ab_dtype_size
+    B_footprint = B_sg_shape[0] * B_sg_shape[1] * ab_dtype_size
+    C_footprint = C_sg_shape[0] * C_sg_shape[1] * c_dtype_size
+
+    total_footprint = A_footprint + B_footprint + C_footprint
+    if verbose:
+        print(f"A: shape={A_sg_shape}, footprint={A_footprint / 1024:.2f} KB")
+        print(f"B: shape={B_sg_shape}, footprint={B_footprint / 1024:.2f} KB")
+        print(f"C: shape={C_sg_shape}, footprint={C_footprint / 1024:.2f} KB")
+        print(f"Total register footprint: {total_footprint / 1024:.2f} KB")
+
+    nb_parallel_dpas = (sg_tile[0] // DPAS.M) * (sg_tile[1] // DPAS.N)
+    if verbose:
+        print(f"Number of DPAS threads: {nb_parallel_dpas}")
+    nb_dpas_ops = nb_parallel_dpas * (k_tile // DPAS.K)
+    if verbose:
+        print(f"Number of total DPAS ops: {nb_dpas_ops}")
+
+    if nb_parallel_dpas > gpu_specs["dpas_exec_size"]:
+        raise ValueError(
+            f"Number of parallel DPAS ops ({nb_parallel_dpas}) exceeds hardware execution size ({gpu_specs['dpas_exec_size']})."
+        )
+
+    # estimate number of used registers
+    reg_size = 64  # bytes per register
+    nb_reg = int((A_footprint + B_footprint + C_footprint) / reg_size)
+    if verbose:
+        print(f"Number of registers: {nb_reg}")
+
+    if nb_reg > gpu_specs["nb_registers"]:
+        raise ValueError(
+            f"Number of registers ({nb_reg}) exceeds hardware register file size ({gpu_specs['nb_registers']})."
+        )
+
+    # # arithmetic intensity
+    # f = (sg_tile[0] * sg_tile[1]) / (sg_tile[0] + sg_tile[1])
+    # ai = f * ab_dtype_size
+    # print(f"Arithmetic intensity: {ai:.2f} FLOPs/Byte")
+    # print(f"Roofline threshold:   {roofline_threshold:.2f} FLOPs/Byte")
+
+    # # is this compute or bandwidth bound?
+    # if ai < roofline_threshold:
+    #     print(" => Bandwidth-bound regime")
+    # else:
+    #     print(" => Compute-bound regime")
+
+    if prefetch_tile_a:
+        # check that prefetch tile is suitable for WG-k tile
+        check_prefetch_tile_a(prefetch_tile_a, wg_tile, k_tile, gpu_specs)
+
+    if prefetch_tile_b:
+        # check that prefetch tile is suitable for WG-k tile
+        check_prefetch_tile_b(prefetch_tile_b, wg_tile, k_tile, gpu_specs)
+
+    return predicted_throughput
+
+
+def generate_configs(M, N, K, gpu_specs, load_strategy="dpas", pf_strategy="best"):
+    """Generate valid tile size configurations based on the selection strategy.
+
+    load_strategy: sets the load tile selection strategy
+    - "large": use the largest supported load tile
+    - "dpas": use dpas op A/B tile size as load tile
+
+    pf_strategy: sets the prefetch tile selection strategy
+    - "best": only the best prefetch tile for A and B based on number of threads
+    - "all": append all valid prefetch tiles for A and B
+
+    Returns:
+    A list of (perf_estimate, params_dict) tuples sorted by perf_estimate (descending).
+    """
+
+    # TODO add tile size strategy "best-equal": best wg/sg/k cases with equal perf
+    # TODO add "heuristic" load tile selection: [16, 32]**2
+    # TODO default heuristic: "best-equal", "heuristic" load and "best" prefetch
+    #      for each wg/sg/k case, 4x load tiles and 1 prefetch tile
+    # TODO add nb_prefetch strategy ?? [1, 2]**2 is 4 configs => 16x total
+    # TODO check if prefetch layout actually affects the performance (same nb threads)
+    # TODO add data type as variable, dpas tiles should be in gpu_specs ?
+
+    # define search space
+    wg_options = [64, 128, 256]
+    sg_options = [32, 64, 128]  # TODO sg min could be 16 ?
+    k_tile_options = [16, 32, 64]
+
+    wg_tiles = product(wg_options, wg_options)
+    sg_tiles = product(sg_options, sg_options)
+
+    def to_param_dict(config):
+        wg_tile, sg_tile, k_tile, ld_a, ld_b, pf_a, pf_b = config
+        return {
+            "m": M,
+            "n": N,
+            "k": K,
+            "wg_m": wg_tile[0],
+            "wg_n": wg_tile[1],
+            "sg_m": sg_tile[0],
+            "sg_n": sg_tile[1],
+            "k_tile": k_tile,
+            "load_a_m": ld_a[0],
+            "load_a_k": ld_a[1],
+            "load_b_k": ld_b[0],
+            "load_b_n": ld_b[1],
+            "prefetch_a_m": pf_a[0],
+            "prefetch_a_k": pf_a[1],
+            "prefetch_b_k": pf_b[0],
+            "prefetch_b_n": pf_b[1],
+            "prefetch_a_nb": 1,
+            "prefetch_b_nb": 1,
+        }
+
+    # grid search
+    valid_configs = []
+    for config in product(wg_tiles, sg_tiles, k_tile_options):
+        wg_tile, sg_tile, k_tile = config
+        try:
+            perf = estimate_perf(
+                M, N, K, wg_tile, sg_tile, k_tile, gpu_specs, verbose=False
+            )
+            if pf_strategy == "best":
+                pf_a, pf_b = generate_prefetch_tiles(wg_tile, k_tile, gpu_specs, n=1)
+                pf_a_list = [pf_a]
+                pf_b_list = [pf_b]
+            else:
+                pf_a_list, pf_b_list = generate_prefetch_tiles(
+                    wg_tile, k_tile, gpu_specs
+                )
+            if load_strategy == "large":
+                load_a_list = [(32, 32)]
+                load_b_list = [(32, 32)]
+            else:
+                load_a_list = [DPAS.A_TILE]
+                load_b_list = [DPAS.B_TILE]
+            for la, lb, pa, pb in product(
+                load_a_list, load_b_list, pf_a_list, pf_b_list
+            ):
+                c = (wg_tile, sg_tile, k_tile, la, lb, pa, pb)
+                params = to_param_dict(c)
+                if check_constraints(params, verbose=False):
+                    valid_configs.append((perf, params))
+        except ValueError:
+            pass
+
+    # sort by performance (descending)
+    valid_configs.sort(key=lambda x: x[0], reverse=True)
+
+    return valid_configs
+
+
+if __name__ == "__main__":
+    M = 4096
+    N = 4096
+    K = 4096
+
+    device = "B70"
+
+    # TODO add data types as inputs
+
+    gpu_specs = gpu_specs_db[device]
+
+    print(f"Matrix multiply M={M}, N={N}, K={K} on {gpu_specs['name']}")
+
+    # tile sizes
+    # wg_tile = [256, 256]  # M, N
+    # k_tile = 32
+    # sg_tile = [64, 32]  # M, N
+    # prefetch_tile_a = [16, 16]
+    # prefetch_tile_b = [16, 32]
+    # perf = estimate_perf(M, N, K, wg_tile, sg_tile, k_tile, gpu_specs, verbose=False,
+    #                      prefetch_tile_a=prefetch_tile_a, prefetch_tile_b=prefetch_tile_b)
+    # print(f"Estimated performance: {perf/1e12:.2f} TFLOPS")
+
+    # print(get_int_factors(32))
+    # pf_a_list, pf_b_list = generate_prefetch_tiles(wg_tile, k_tile)
+    # print("Valid prefetch tiles for A:")
+    # for pf_a in pf_a_list:
+    #     print(pf_a)
+    # print("Valid prefetch tiles for B:")
+    # for pf_b in pf_b_list:
+    #     print(pf_b)
+    # exit(0)
+
+    configs = generate_configs(M, N, K, gpu_specs)
+
+    print(f"Found {len(configs)} valid configurations.")
+
+    nprint = 50
+    nprint = min(nprint, len(configs))
+    print(f"\nBest {nprint} configurations:")
+    for perf, params in configs[:nprint]:
+        wg_tile = (params["wg_m"], params["wg_n"])
+        sg_tile = (params["sg_m"], params["sg_n"])
+        k_tile = params["k_tile"]
+        ld_a = (params["load_a_m"], params["load_a_k"])
+        ld_b = (params["load_b_k"], params["load_b_n"])
+        pf_a = (params["prefetch_a_m"], params["prefetch_a_k"])
+        pf_b = (params["prefetch_b_k"], params["prefetch_b_n"])
+
+        def to_str(iterable):
+            return " ".join(str(x) for x in iterable)
+
+        config = "  ".join(
+            [
+                f"WG: {to_str(wg_tile):7s}",
+                f"SG: {to_str(sg_tile):5s}",
+                f"K: {k_tile:2d}",
+                f"LD_A: {to_str(ld_a):5s}",
+                f"LD_B: {to_str(ld_b):5s}",
+                f"PF_A: {to_str(pf_a):5s}",
+                f"PF_B: {to_str(pf_b):5s}",
+            ]
+        )
+        print(f"{config} => {perf / 1e12:.2f} TFLOPS")
---
+++ examples/xegpu/tune_matmul_costmodel.py
@@ -0,0 +1,131 @@
+# TODO add lit tests or skip
+
+from time import perf_counter
+from datetime import timedelta
+import os
+import sys
+from csv_logger import CSVLogger
+
+from matmul import cli_parser
+from tune_utils import dump_configs_json, execute_and_log
+from tilesize_selector import generate_configs, gpu_specs_db
+from tune_matmul_gridsearch import check_constraints, run_experiment
+
+
+if __name__ == "__main__":
+    parser = cli_parser(
+        description="Optimize matmul kernel parameters using a cost model search."
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Check validity of combinations but do not execute kernels.",
+    )
+    parser.add_argument(
+        "--target",
+        choices=["B70", "B580"],
+        default="B70",
+        help="Target GPU device.",
+    )
+    parser.add_argument(
+        "--max-iters",
+        type=int,
+        help="Maximum number of executed configurations.",
+    )
+    parser.add_argument(
+        "--no-check-result",
+        action="store_true",
+        help="Skip correctness check.",
+    )
+    parser.add_argument(
+        "--dump-json",
+        dest="n_dump_json",
+        type=int,
+        default=0,
+        help="Dump the best n configurations as JSON files.",
+    )
+    args = parser.parse_args()
+
+    sizes = args.sizes
+    has_bias = args.bias
+    has_relu = args.relu
+    accumulate_c = not args.no_accumulate_c
+    ab_type = "f16"
+    c_type = "f32"
+
+    # timeout for kernel execution in seconds
+    timeout = 50
+
+    # number of iterations in kernel timing is chosen adaptively
+    nwarmup = None
+    nruns = None
+
+    # disable IGC compiler cache
+    os.environ["NEO_CACHE_PERSISTENT"] = "0"
+
+    if not args.dry_run:
+        csv_file = "out_costmodel.csv"
+        csv_logger = CSVLogger(csv_file)
+
+    gpu_specs = gpu_specs_db[args.target]
+
+    print(f"Matmul problem size: {sizes}")
+    print(f"device={gpu_specs['name']}")
+    print(f"{ab_type=}")
+    print(f"{c_type=}")
+    print(f"{has_bias=}")
+    print(f"{has_relu=}")
+    print(f"{accumulate_c=}")
+    sys.stdout.flush()
+
+    configs = generate_configs(*sizes, gpu_specs)
+    print(f"Total complexity: {len(configs)} configurations")
+
+    i = 0
+    executed_configs = []
+    tic = perf_counter()
+    for perf_estimate, params in configs:
+        if not check_constraints(params, verbose=True):
+            print(f"Skipping invalid configuration: {params}")
+            continue
+
+        i += 1
+        if args.max_iters is not None and i >= args.max_iters:
+            print(f"Reached maximum number of iterations: {args.max_iters}")
+            break
+        if args.dry_run:
+            continue
+        time, gflops = execute_and_log(
+            run_experiment,
+            csv_logger,
+            nruns,
+            nwarmup,
+            params,
+            check_result=not args.no_check_result,
+            timeout=timeout,
+            ab_type=ab_type,
+            c_type=c_type,
+            has_bias=has_bias,
+            has_relu=has_relu,
+            accumulate_c=accumulate_c,
+        )
+        executed_configs.append((gflops, params))
+
+    duration = perf_counter() - tic
+    print(f"Number of executed configurations: {i}")
+    print(f"Total duration: {timedelta(seconds=duration)}")
+
+    if args.n_dump_json > 0 and not args.dry_run:
+        executed_configs.sort(key=lambda x: x[0], reverse=True)
+        best_configs = [c for c in executed_configs[: args.n_dump_json]]
+        print("Best configurations found:")
+        for gflops, params in best_configs:
+            print(f" GFLOPS: {gflops:.2f}: {list(params.values())}")
+        sizes_str = "-".join(str(s) for s in sizes)
+        relu_str = "_relu" if has_relu else ""
+        bias_str = "_bias" if has_bias else ""
+        acc_str = "_acc" if accumulate_c else ""
+        prefix = (
+            f"matmul_params_{sizes_str}_{ab_type}-{c_type}{bias_str}{relu_str}{acc_str}"
+        )
+        dump_configs_json([p for _, p in best_configs], filename_prefix=prefix)
