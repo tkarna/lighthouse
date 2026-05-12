cb443a1f9c76dff463f8c406782369704794915f
Tuomas Karna <tuomas.karna@intel.com>
2026-05-12 20:05:35 +0300

    tmp: tune_matmul_gridsearch: use lh check constraints

--- examples/xegpu/tune_matmul_gridsearch.py
+++ examples/xegpu/tune_matmul_gridsearch.py
@@ -16,6 +16,7 @@ from lighthouse import dialects as lh_dialects
 from lighthouse.execution.runner import Runner
 from lighthouse.schedule.xegpu.mlp_schedule import DPAS
 from lighthouse.pipeline.driver import TransformDriver
+from lighthouse.schedule.xegpu.xegpu_costmodel import check_constraints
 
 from matmul import XeGPUMatMul, check_results, cli_parser
 from genetic_algorithm import (
@@ -23,16 +24,6 @@ from genetic_algorithm import (
     VariableSet,
 )
 from tune_utils import dump_configs_json, execute_and_log
-from lighthouse.schedule.xegpu.mlp_schedule import (
-    MAX_NB_SG_THREADS,
-    LOAD_MAX_COLS,
-    LOAD_MAX_ROWS,
-    PFETCH_MAX_COLS,
-    PFETCH_MAX_ROWS,
-    PFETCH_MIN_COLS,
-    PFETCH_MIN_ROWS,
-    MIN_NB_THREADS,
-)
 
 
 def run_experiment(
@@ -107,156 +98,6 @@ def run_experiment(
     gflops = flop_count / (elapsed * 1e-6) / 1e9
 
     return elapsed, gflops
-
-
-def check_constraints(params: dict, verbose: bool = False) -> bool:
-    def print_reason(msg):
-        if verbose:
-            print(f"  Invalid: {msg}")
-
-    M = params["m"]
-    N = params["n"]
-    wg_tile_m = params["wg_m"]
-    wg_tile_n = params["wg_n"]
-    sg_tile_m = params["sg_m"]
-    sg_tile_n = params["sg_n"]
-    load_tile_a_m = params["load_a_m"]
-    load_tile_a_k = params["load_a_k"]
-    load_tile_b_k = params["load_b_k"]
-    load_tile_b_n = params["load_b_n"]
-    prefetch_tile_a_m = params["prefetch_a_m"]
-    prefetch_tile_a_k = params["prefetch_a_k"]
-    prefetch_tile_b_k = params["prefetch_b_k"]
-    prefetch_tile_b_n = params["prefetch_b_n"]
-    k_tile = params["k_tile"]
-
-    if M % wg_tile_m != 0:
-        print_reason("wg_tile_m does not divide M")
-        return False
-    if N % wg_tile_n != 0:
-        print_reason("wg_tile_n does not divide N")
-        return False
-    if wg_tile_m % sg_tile_m != 0:
-        print_reason("sg_tile_m does not divide wg_tile_m")
-        return False
-    if wg_tile_n % sg_tile_n != 0:
-        print_reason("sg_tile_n does not divide wg_tile_n")
-        return False
-    if sg_tile_m % DPAS.M != 0:
-        print_reason("sg_tile_m not multiple of dpas_m")
-        return False
-    if sg_tile_n % DPAS.N != 0:
-        print_reason("sg_tile_n not multiple of dpas_n")
-        return False
-    if k_tile % DPAS.K != 0:
-        print_reason("k_tile not multiple of dpas_k")
-        return False
-
-    # SG level thread layout: [nb_sg_threads_m, nb_sg_threads_n]
-    nb_sg_threads_m = wg_tile_m // sg_tile_m
-    nb_sg_threads_n = wg_tile_n // sg_tile_n
-    nb_sg_threads = nb_sg_threads_m * nb_sg_threads_n
-    if nb_sg_threads > MAX_NB_SG_THREADS:
-        print_reason("too many sg threads")
-        return False
-    if nb_sg_threads < MIN_NB_THREADS:
-        print_reason("too few sg threads")
-        return False
-
-    if sg_tile_m % load_tile_a_m != 0:
-        print_reason("load_tile_a_m does not divide sg_tile_m")
-        return False
-    if k_tile % load_tile_a_k != 0:
-        print_reason("load_tile_a_k does not divide k_tile")
-        return False
-    if k_tile % load_tile_b_k != 0:
-        print_reason("load_tile_b_k does not divide k_tile")
-        return False
-    if sg_tile_n % load_tile_b_n != 0:
-        print_reason("load_tile_b_n does not divide sg_tile_n")
-        return False
-    if load_tile_a_m > LOAD_MAX_ROWS:
-        print_reason("too large load_tile_a_m")
-        return False
-    if load_tile_a_k > LOAD_MAX_COLS:
-        print_reason("too large load_tile_a_k")
-        return False
-    if load_tile_b_k > LOAD_MAX_ROWS:
-        print_reason("too large load_tile_b_k")
-        return False
-    if load_tile_b_n > LOAD_MAX_COLS:
-        print_reason("too large load_tile_b_n")
-        return False
-    if sg_tile_m % prefetch_tile_a_m != 0:
-        print_reason("prefetch_tile_a_m does not divide sg_tile_m")
-        return False
-    if k_tile % prefetch_tile_a_k != 0:
-        print_reason("prefetch_tile_a_k does not divide k_tile")
-        return False
-    if k_tile % prefetch_tile_b_k != 0:
-        print_reason("prefetch_tile_b_k does not divide k_tile")
-        return False
-    if sg_tile_n % prefetch_tile_b_n != 0:
-        print_reason("prefetch_tile_b_n does not divide sg_tile_n")
-        return False
-    if prefetch_tile_a_m > PFETCH_MAX_ROWS:
-        print_reason("too large prefetch_tile_a_m")
-        return False
-    if prefetch_tile_a_k > PFETCH_MAX_COLS:
-        print_reason("too large prefetch_tile_a_k")
-        return False
-    if prefetch_tile_b_k > PFETCH_MAX_ROWS:
-        print_reason("too large prefetch_tile_b_k")
-        return False
-    if prefetch_tile_b_n > PFETCH_MAX_COLS:
-        print_reason("too large prefetch_tile_b_n")
-        return False
-    if prefetch_tile_a_m < PFETCH_MIN_ROWS:
-        print_reason("too small prefetch_tile_a_m")
-        return False
-    if prefetch_tile_a_k < PFETCH_MIN_COLS:
-        print_reason("too small prefetch_tile_a_k")
-        return False
-    if prefetch_tile_b_k < PFETCH_MIN_ROWS:
-        print_reason("too small prefetch_tile_b_k")
-        return False
-    if prefetch_tile_b_n < PFETCH_MIN_COLS:
-        print_reason("too small prefetch_tile_b_n")
-        return False
-    if load_tile_a_m % DPAS.M != 0:
-        print_reason("load_tile_a_m not multiple of dpas_m")
-        return False
-    if load_tile_a_k % DPAS.K != 0:
-        print_reason("load_tile_a_k not multiple of dpas_k")
-        return False
-    if load_tile_b_k % DPAS.K != 0:
-        print_reason("load_tile_b_k not multiple of dpas_k")
-        return False
-    if load_tile_b_n % DPAS.N != 0:
-        print_reason("load_tile_b_n not multiple of dpas_n")
-        return False
-
-    # prefetch A layout
-    nb_prefetch_a_m = wg_tile_m // prefetch_tile_a_m
-    nb_prefetch_a_k = k_tile // prefetch_tile_a_k
-    if nb_prefetch_a_m * nb_prefetch_a_k > MAX_NB_SG_THREADS:
-        print_reason("too many prefetch A tiles")
-        return False
-    if nb_prefetch_a_m * nb_prefetch_a_k < MIN_NB_THREADS:
-        print_reason("too few prefetch A threads")
-        return False
-
-    # prefetch B layout
-    nb_prefetch_b_k = k_tile // prefetch_tile_b_k
-    nb_prefetch_b_n = wg_tile_n // prefetch_tile_b_n
-    if nb_prefetch_b_k * nb_prefetch_b_n > MAX_NB_SG_THREADS:
-        print_reason("too many prefetch B tiles")
-        return False
-    if nb_prefetch_b_k * nb_prefetch_b_n < MIN_NB_THREADS:
-        print_reason("too few prefetch B threads")
-        return False
-
-    return True
 
 
 def get_divisors(n: int, min_tile: int = 32, max_tile: int = 256) -> list[int]:
