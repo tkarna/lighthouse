6af02b3172691b9162ba385d9d610c0ece249fc4
Tuomas Karna <tuomas.karna@intel.com>
2026-05-19 13:20:23 +0300

    tmp: update xegpu device dicts

--- lighthouse/schedule/xegpu/xegpu_devices.py
+++ lighthouse/schedule/xegpu/xegpu_devices.py
@@ -3,16 +3,21 @@ __all__ = ["gpu_specs_db"]
 
 # GPU specs
 
+gpu_bmg_common = {
+    "dpas_exec_size": 16,  # number of parallel dpas ops in sg
+    "max_nb_threads": 32,  # large register file
+    "nb_registers": 256,  # large register file
+    # "max_nb_threads": 64  # small register file
+    # "nb_registers": 128,  # small register file
+}
+
 # Intel Arc B70
 gpu_specs_B70 = {
     "name": "Intel Arc B70",
     "nb_xe_cores": 32,
     "peak_flops": 155000e9,  # float16
     "bw_global_mem": 608e9,  # GB/s
-    "dpas_exec_size": 16,  # number of parallel dpas ops in sg
-    "max_nb_threads": 32,  # large register file
-    # "max_nb_threads": 64  # small register file
-    "nb_registers": 256,  # large register file
+    **gpu_bmg_common,
 }
 
 # Intel Arc B580
@@ -21,10 +26,7 @@ gpu_specs_B580 = {
     "nb_xe_cores": 20,
     "peak_flops": 100000e9,  # float16
     "bw_global_mem": 456e9,  # GB/s
-    "max_nb_threads": 32,  # large register file
-    "dpas_exec_size": 16,  # number of parallel dpas ops in sg
-    # "max_nb_threads": 64,  # small register file
-    "nb_registers": 256,  # large register file
+    **gpu_bmg_common,
 }
 
 gpu_specs_db = {
