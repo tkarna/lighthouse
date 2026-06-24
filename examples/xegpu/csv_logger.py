d51de25c0b46afa9f2c94761885de93dc235eded
Tuomas Karna <tuomas.karna@intel.com>
2026-06-24 20:27:09 +0300

    CSVLogger: add echo_stdout and verbose options

--- examples/xegpu/csv_logger.py
+++ examples/xegpu/csv_logger.py
@@ -4,7 +4,9 @@ import os
 
 
 class CSVLogger:
-    def __init__(self, filename: str = None):
+    def __init__(
+        self, filename: str = None, echo_stdout: bool = True, verbose: bool = False
+    ):
         def gen_unique(path):
             base, ext = os.path.splitext(path)
             i = 1
@@ -14,23 +16,28 @@ class CSVLogger:
             return path
 
         self.filename = gen_unique(filename) if filename is not None else None
+        if verbose:
+            print(f"Logging to file: {self.filename}")
         self.header_written = False
         self.fieldnames = None
-        self.logger = logging.getLogger(
-            "csv_logger_" + (self.filename if self.filename else "stdout")
-        )
-        self.logger.setLevel(logging.INFO)
-        if not self.logger.hasHandlers():
-            handler = logging.StreamHandler()
-            handler.setFormatter(logging.Formatter("%(message)s"))
-            self.logger.addHandler(handler)
+        self.logger = None
+        if echo_stdout:
+            self.logger = logging.getLogger(
+                "csv_logger_" + (self.filename if self.filename else "stdout")
+            )
+            self.logger.setLevel(logging.INFO)
+            if not self.logger.hasHandlers():
+                handler = logging.StreamHandler()
+                handler.setFormatter(logging.Formatter("%(message)s"))
+                self.logger.addHandler(handler)
 
     def log(self, data: dict):
         if self.fieldnames is None:
             self.fieldnames = list(data.keys())
-        if not self.header_written:
-            self.logger.info(",".join(self.fieldnames))
-        self.logger.info(",".join(str(data[k]) for k in self.fieldnames))
+        if self.logger is not None:
+            if not self.header_written:
+                self.logger.info(",".join(self.fieldnames))
+            self.logger.info(",".join(str(data[k]) for k in self.fieldnames))
         if self.filename is not None:
             with open(self.filename, mode="a", newline="") as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
